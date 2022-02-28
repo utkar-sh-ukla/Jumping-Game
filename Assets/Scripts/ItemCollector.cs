3e800d9edb775f404b45e757473fd0d20dc68bbd
Utkarsh-Shukla-Github <utkarshshukla707@gmail.com>
2022-02-28 21:47:23 +0530

    sounds

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -8,11 +8,13 @@ public class ItemCollector : MonoBehaviour
     private int cherries = 0;
 
     [SerializeField] private Text cherriesText;
+    [SerializeField] private AudioSource collectionSoundEffect;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Cherry"))
         {
+            collectionSoundEffect.Play();
             Destroy(collision.gameObject);
             cherries++;
             cherriesText.text = "Cherries: " + cherries;
